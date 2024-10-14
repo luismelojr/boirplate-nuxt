919192d732ac9d4c75a7e97e74ee5b16d2c8d8e0
Luis Henrique <junimhs1996@gmail.com>
2024-10-14 10:10:08 -0300

    chore(project): install tailwindcss and primevue

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from 'primevue/button';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-primary-500">Hello</h1>
+    <Button label="Teste" size="small"/>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path'
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  primevue: {
+    options: {
+      unstyled: true
+    },
+    importPT: {from: path.resolve(__dirname, './assets/presets/lara')}
+  }
+})
